a37a1df23db0fe5e696cbeb8110e3813a76cdfaf
Nick Kavunenko <webjema@gmail.com>
2017-05-21 22:40:12 +0300

    Read screen argument example is added

---
+++ Demo1/Scripts/GameBoardScreenArguments.cs
@@ -0,0 +1,25 @@
+﻿using com.webjema.PanelsMonster;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBoardScreenArguments : IScreenArguments
+{
+
+    private int _levelIndex;
+
+    public GameBoardScreenArguments(int arguments)
+    {
+        this._levelIndex = arguments;
+    }
+
+    public object GetScreenArguments()
+    {
+        return this._levelIndex;
+    }
+
+    public int GetTypedScreenArguments()
+    {
+        return this._levelIndex;
+    }
+}
---
+++ Demo1/Scripts/GameBoardScreenStartPoint.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using com.webjema.PanelsMonster;
+
+public class GameBoardScreenStartPoint : MonoBehaviour {
+
+    // GameBoard Screen start point is here
+    void Start () {
+        ScreensManager.Instance.CurrentScreenArguments
+            .MatchAction(
+                (a) => {
+                    GameBoardScreenArguments args = a as GameBoardScreenArguments;
+                    this.InitLevel(args.GetTypedScreenArguments());
+                }, 
+                () => {
+                    Debug.LogError("[GameBoardScreenStartPoint] arguments is required to start");
+                }
+            );
+    } // Start
+
+    void InitLevel(int index)
+    {
+        Debug.Log("[InitLevel] index = " + index);
+
+        // just to show something
+        PanelsManager.Instance.GetPanel("FakeGamePanel")
+            .SetProperty(PanelPropertyName.title, string.Format("Level {0} start", index), PanelPropertyType.text)
+            .Show();
+    }
+
+} // GameBoardScreenStartPoint
